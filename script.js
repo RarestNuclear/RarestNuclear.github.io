07a05cb4bc528e66ef59681e86497f1a1cc00948
RarestNuclear <rory.lacourciere@gmail.com>
2026-05-29 21:25:34 -0700

    n
    j

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+ document.query.Selector('img'); 
+  if
+    (lightbulb.src.endsWith('4d26vbz5'))
+  {
+    lightbulb.src ='https://tinyurl.com/4225uhzk';
+  } else {
+     lightbulb.src ='https://tinyurl.com/4d26vbz5';
+  }  }
+
+  let imageElement = document.querySelector('img');
+if (imageElement) { imageElement.addEventListener('Click', toggleLight);
+                  }});)
